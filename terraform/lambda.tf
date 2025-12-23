6452c393ca9751347416290f30eabce7b7da77e3
Nikоlаy Dimitrov <73880661+ngdimitrov@users.noreply.github.com>
2025-12-23 15:33:33 +0200

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,3 +1,23 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "serverless_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "gemini_api" {
   filename      = "../gemini.zip"
   function_name = "geminiChatFunction"
@@ -11,6 +31,22 @@ resource "aws_lambda_function" "gemini_api" {
   environment {
     variables = {
       GEMINI_API_KEY = var.gemini_api_key
+    }
+  }
+}
+
+resource "aws_lambda_function" "resend_api" {
+  filename      = "../resend.zip"
+  function_name = "sendEmailFunction"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs22.x"
+  timeout       = 4
+
+  source_code_hash = filebase64sha256("../resend.zip")
+
+  environment {
+    variables = {
       RESEND_API_KEY = var.resend_api_key
     }
   }
