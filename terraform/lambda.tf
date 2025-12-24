6244c5a847aa5c3e6853554a7b386a3f495a8bf9
Nikоlаy Dimitrov <73880661+ngdimitrov@users.noreply.github.com>
2025-12-24 08:35:47 +0200

    Add KMS decrypt policy to Lambda IAM role
    Introduces a new IAM role policy allowing Lambda functions to perform KMS decryption. Also removes npm install from Lambda packaging in the deploy workflow, likely assuming dependencies are already present or handled elsewhere.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -13,6 +13,24 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
+resource "aws_iam_role_policy" "lambda_kms" {
+  name = "lambda-kms-decrypt"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "kms:Decrypt"
+        ]
+        Resource = "*" 
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
